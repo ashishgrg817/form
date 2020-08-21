0140ae9728ebcf679a2976f82eed21ffa8ab75e8
Ashish <ashishgrg.817@gmail.com>
2020-08-21 21:40:42 +0530

    first commit

---
+++ src/app/hero-form/hero-form.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-hero-form',
+  templateUrl: './hero-form.component.html',
+  styleUrls: ['./hero-form.component.css']
+})
+export class HeroFormComponent {
+
+  powers = ['Really Smart', 'Super Flexible',
+            'Super Hot', 'Weather Changer'];
+
+  model = new Hero(18, '', this.powers[0], '');
+
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.model); }
+
+  
+
+
+  //////// NOT SHOWN IN DOCS ////////
+
+  // Reveal in html:
+  //   Name via form.controls = {{showFormControls(heroForm)}}
+  showFormControls(form: any) {
+    return form && form.controls.name &&
+    form.controls.name.value; // Dr. IQ
+  }
+
+  /////////////////////////////
+
+}
